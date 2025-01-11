60c4400be1468c0b0392b2f411b5cba5325811fd
Julianoland <juliannojp@gmail.com>
2025-01-11 18:47:50 -0300

    pequenas modificações

---
+++ public/assets/js/bundle.js
@@ -0,0 +1,13 @@
+/******/ (() => { // webpackBootstrap
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+//a
+//b
+//c
+
+console.log('testando arquivo comprimido');
+console.log('curso de programação web');
+/******/ })()
+;
+//# sourceMappingURL=bundle.js.map
---
+++ src/index.js
@@ -0,0 +1,6 @@
+//a
+//b
+//c
+
+console.log('testando arquivo comprimido');
+console.log('curso de programação web');
