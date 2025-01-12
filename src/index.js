f1e6996076a214dd0dd139adc69fc2ce6906454d
Julianoland <juliannojp@gmail.com>
2025-01-11 22:44:31 -0300

    integração com css no WP, organização de pastas

--- src/index.js
+++
@@ -1,6 +0,0 @@
-//a
-//b
-//c
-
-console.log('testando arquivo comprimido');
-console.log('curso de programação web');
--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,25 @@
-const path = require('path');
+const path = require('path'); // ferramenta do Node.js para lidar com caminhos de arquivos.
 
 module.exports = {
-    mode: 'development',
-    entry: './src/index.js',
+    mode: 'development', // Define o modo de execução do código - development / production 
+    entry: './src/main.js', // Ponto de entrada do projeto (arquivo principal)
     output: {
-        path: path.resolve(__dirname, 'public', 'assets', 'js'),
-        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'public', 'assets', 'js'), // Pasta de saida para o pacote
+        filename: 'bundle.js', // Nome do arquivo final
     },
     module:{
         rules: [{
-            exclude: /node_modules/,
-            test: /\.js$/,
+            exclude: /node_modules/, // Ignora a pasta node_modules
+            test: /\.js$/, // Alvo: arquivos .js
             use: {
-                loader:'babel-loader',
+                loader:'babel-loader', //Usa Babel para esses arquivos
                 options: {
-                    presets:['@babel/env']
+                    presets:['@babel/env'] // Usa o preset para transformar o JS moderno
                 }
             }
+        },{
+            test: /\.css$/,
+            use: ['style-loader', 'css-loader']
         }]
     },
     devtool: 'source-map', //mapeamento de erros e logs do bundle
