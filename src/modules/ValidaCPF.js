3d3b91c7f39519fab38010d20080294b0d3ed627
Julianoland <juliannojp@gmail.com>
2025-01-13 21:40:49 -0300

    adicionando comentários para facil compreenção

--- src/modules/ValidaCPF.js
+++ src/modules/ValidaCPF.js
@@ -1,3 +1,4 @@
+// Para validarmos um CPF vamos iniciar retirando os caracteres especiais
 class ValidaCPF {
     constructor(cpfEnviado) {
         Object.defineProperty(this, 'cpfLimpo', {
@@ -9,18 +10,56 @@ class ValidaCPF {
         });
     }
 
-    // validação para verificar se temos numeros repetidos
+    // validação para verificar se temos numeros repetidos, verificação na função de validação
     sequencia() {
         return this.cpfLimpo.charAt(0).repeat(11) === this.cpfLimpo;
     }
 
+    // função para remoção dos 2 ultimos digitos
+    geraNovoCPF(){
+        // pegamos o cpf e removemos os 2 ultimos digitos
+        const cpfSemDigito = this.cpfLimpo.slice(0, -2);
+        // gera o primeiro digito enviando o cpf sem digito para outra função
+        const digito1 = this.geraDigito(cpfSemDigito);
+    }
+
+    // função para criação de digito
+    geraDigito(cpf){
+        /*
+        PRIMEIRO DIGITO
+        - multiplica cada digito do cpf (os 9 primeiros) com a sequencia decrescente de 10.
+            Ex: para o CPF 123456789, multiplica 1*10 2*9 3*8 e assim por diante...
+        - soma o resultado de cada multiplicação, podemos colocar o resultado em TOTAL, por exemplo.
+        - dividimos o resultado com a qtd de numeros do CPF, ou seja, 11. Aqui o que queremos é o resto da divisão então utilizaremos o % (modulo).
+        - subtrairmos o resto da divisão por 11 e chegamos ao primeiro digito verificador.
+            Lembrando que se o resultado for maior que 9 então considerar o valor do digito como '0'.
+        */
+
+        let total = 0;
+        let posicao = cpf.length + 1; // 10
+
+
+        for(let numString of cpf) {
+            total += posicao * Number(numString);
+            posicao--;
+        }
+    }
+
+    // O coração da classe onde obtiremos o resultado de tudo
     valida() {
+        // Lembrando que aqui os dados já estao sem os caracteres especiais.
+
+        // Se não houver nenhum dado, não continue...
         if(!this.cpfLimpo) return false;
+        // Se for diferente de string, como exemplo só número, não continue...
         if(typeof this.cpfLimpo !== 'string') return false;
+        // Se for tamanho diferente de 11 digitos, não continue...
         if(this.cpfLimpo.length !==  11) return false;
+        // Se for uma sequencia, ou seja, todos os numeros iguais, não continue...
         if(this.sequencia()) return false;
 
-
+        // Vamos gerar os novos digitos para comparação com o CPF enviado
+        this.geraNovoCPF();
     }
 }
 
